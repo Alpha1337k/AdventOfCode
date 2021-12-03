f78678594b6bc722cda63d756f83db4803bd7d71
Alpha1337k <ozziekruithof@gmail.com>
2021-12-03 06:39:16 +0100

    Feat: added node and sample files to script

--- 2021/createTs.sh
+++ 2021/createTs.sh
@@ -3,5 +3,6 @@ npm install typescript --save-dev
 npx tsc --init --rootDir src --outDir dist --sourceMap true
 npm set-script start "npx tsc && node dist/main.js"
 npm set-script watch "tsc-watch --onSuccess 'node ./dist/main.js'"
+npm install --save-dev @types/node
 mkdir src
-touch src/main.ts
+touch src/main.ts input.txt test.txt
